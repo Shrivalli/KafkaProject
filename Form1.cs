bccb3b89c854ed3490390d7888c2702eb1fc6579
Shrivalli Maheshwaran <shrivalli04@gmail.com>
2021-02-06 10:44:41 +0530

    Kafka Eg Added

---
+++ Form1.cs
@@ -0,0 +1,64 @@
+﻿using KafkaNet;
+using KafkaNet.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KafkaEg
+{
+    public partial class Form1 : Form
+    {
+        Uri uri;
+        string topic;
+        public Form1()
+        {
+            InitializeComponent();
+            uri = new Uri("http://localhost:9092");
+            topic = "chat-message";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == string.Empty)
+            {
+                MessageBox.Show("Please Enter Message", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            string payload = textBox1.Text.Trim();
+            var sendMessage = new Thread(() => {
+                KafkaNet.Protocol.Message msg = new KafkaNet.Protocol.Message(payload);
+                var options = new KafkaOptions(uri);
+                var router = new BrokerRouter(options);
+                var client = new Producer(router);
+                client.SendMessageAsync(topic, new List<KafkaNet.Protocol.Message> { msg }).Wait();
+            });
+            sendMessage.Start();
+            //this.Clear();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == string.Empty)
+            {
+                MessageBox.Show("Please Enter Message", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            string payload = textBox1.Text.Trim();
+            var sendMessage = new Thread(() => {
+                KafkaNet.Protocol.Message msg = new KafkaNet.Protocol.Message(payload);
+                var options = new KafkaOptions(uri);
+                var router = new BrokerRouter(options);
+                var client = new Producer(router);
+                client.SendMessageAsync(topic, new List<KafkaNet.Protocol.Message> { msg }).Wait();
+            });
+            sendMessage.Start();
+        }
+    }
+}
